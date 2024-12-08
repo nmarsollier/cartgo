e3a356cf9bd9a33f220d68d181ee6d6570869eee
Nestor Marsollier <nmarsollier@gmail.com>
2024-12-08 11:28:31 -0300

    Update generate_mocks.sh

--- scripts/generate_mocks.sh
+++ scripts/generate_mocks.sh
@@ -1,4 +1,3 @@
 mockgen -source=./tools/httpx/client.go -destination=./tools/httpx/client_mocks.go -package=httpx
 mockgen -source=./rabbit/emit/rabbit_channel.go -destination=./rabbit/emit/rabbit_channel_mocks.go -package=emit
-mockgen -source=./tools/db/mongo_collection.go -destination=./tools/db/mongo_collection_mocks.go -package=db
 mockgen -source=./tools/log/logrus_logger.go -destination=./tools/log/logrus_logger_mocks.go -package=log
