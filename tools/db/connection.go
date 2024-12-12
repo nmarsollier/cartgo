76ac4851f91ad0d52c4c044876f131aab0357fcd
Nestor Marsollier <nmarsollier@gmail.com>
2024-12-12 18:51:13 -0300

    update

--- tools/db/connection.go
+++ tools/db/connection.go
@@ -2,8 +2,10 @@ package db
 
 import (
 	"context"
+	"errors"
 	"reflect"
 
+	"github.com/jackc/pgx"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/nmarsollier/cartgo/tools/env"
 	"github.com/nmarsollier/cartgo/tools/log"
@@ -14,21 +16,23 @@ var (
 )
 
 func GetPostgresClient(deps ...interface{}) (*pgxpool.Pool, error) {
-	if instance == nil {
-		config, err := pgxpool.ParseConfig(env.Get().PostgresURL)
-		if err != nil {
-			log.Get(deps...).Error(err)
-			return nil, err
-		}
-
-		instance, err = pgxpool.NewWithConfig(context.Background(), config)
-		if err != nil {
-			log.Get(deps...).Error(err)
-			return nil, err
-		}
-
-		log.Get(deps...).Info("Postgres Connected")
+	if instance != nil {
+		return instance, nil
 	}
+
+	config, err := pgxpool.ParseConfig(env.Get().PostgresURL)
+	if err != nil {
+		log.Get(deps...).Error(err)
+		return nil, err
+	}
+
+	instance, err = pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		log.Get(deps...).Error(err)
+		return nil, err
+	}
+
+	log.Get(deps...).Info("Postgres Connected")
 
 	return instance, nil
 }
@@ -46,6 +50,10 @@ func QueryRow[T any](query string, args []interface{}, deps ...interface{}) (res
 	row := conn.QueryRow(context.Background(), query, args...)
 
 	if err = row.Scan(fieldAddrs(destVal)...); err != nil {
+		if isConnectionError(err) {
+			instance = nil
+		}
+
 		log.Get(deps...).Error(err)
 		return
 	}
@@ -59,4 +67,20 @@ func fieldAddrs(destVal reflect.Value) (addrs []interface{}) {
 		addrs[i] = destVal.Field(i).Addr().Interface()
 	}
 	return
+}
+
+func isConnectionError(err error) bool {
+	var pgErr *pgx.PgError
+	if errors.As(err, &pgErr) {
+		switch pgErr.Code {
+		case "08000", "08003", "08006", "08001", "08004", "08007", "08P01":
+			return true
+		}
+	}
+
+	if errors.Is(err, context.DeadlineExceeded) || errors.Is(err, context.Canceled) {
+		return true
+	}
+
+	return false
 }
